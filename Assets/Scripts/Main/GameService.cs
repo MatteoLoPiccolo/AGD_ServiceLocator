f43136d2848c288d796d1615eaaa8000e5af09dd
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-05-08 18:08:20 +0200

    Implementing Service Locator

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,20 @@
+using ServiceLocator.Player;
+using ServiceLocator.Utilities;
+using UnityEngine;
+
+public class GameService : GenericMonoSingleton<GameService>
+{
+    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+
+    public PlayerService playerService {  get; private set; }
+
+    private void Start()
+    {
+        playerService = new PlayerService(playerScriptableObject);
+    }
+
+    private void Update()
+    {
+        playerService.Update();
+    }
+}
